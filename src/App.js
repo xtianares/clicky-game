d55e60236523263e6900b444851b89ed15510f55
xtianares <xtianares@yahoo.com>
2019-04-07 22:22:43 -0400

    initial commit with header and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './scss/App.scss';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Header />
+        <main className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h1 className="h3 mt-4 mb-5">Click as many images as you can without clicking the same one</h1>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </Fragment>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <header>
+        <div className="container-fluid">
+          <div className="row d-flex align-items-center">
+            <div className="col-md-3 title">
+              Clicky Game
+            </div>
+            <div className="col-md-6 text-center">
+              Click an image to begin!
+            </div>
+            <div className="col-md-3 score">
+              Score: 0 | Top Score: 0
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
