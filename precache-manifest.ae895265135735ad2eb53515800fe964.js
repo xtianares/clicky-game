e6523b1f526188f69039ce748d01ec1b8fd0a2d5
xtianares <xtianares@yahoo.com>
2019-09-12 23:00:51 -0400

    Updates

---
+++ precache-manifest.ae895265135735ad2eb53515800fe964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "cd6d5c596aa021f48c34",
+    "url": "/clicky-game/static/js/main.23127efc.chunk.js"
+  },
+  {
+    "revision": "367fd17d0a37fa96ca6b",
+    "url": "/clicky-game/static/js/2.21dc9bd0.chunk.js"
+  },
+  {
+    "revision": "cd6d5c596aa021f48c34",
+    "url": "/clicky-game/static/css/main.2064f5ca.chunk.css"
+  },
+  {
+    "revision": "367fd17d0a37fa96ca6b",
+    "url": "/clicky-game/static/css/2.0d6ff601.chunk.css"
+  },
+  {
+    "revision": "5b231173fac386b67750b96a943742ad",
+    "url": "/clicky-game/index.html"
+  }
+];
